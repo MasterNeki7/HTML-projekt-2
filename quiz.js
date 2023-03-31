f6a813c6190d7aa1cfabf10df364d65ef4cd631b
Neo Babic <neo.babic@elev.ga.ntig.se>
2023-03-31 14:11:46 +0200

    commit:7?

---
+++ quiz.js
@@ -0,0 +1,30 @@
+//----------------------------------- Lets for quiz -----------------------------------
+let points = 0;
+
+
+//----------------------------------- Lets for buttons --------------------------------
+let finishButton;
+let exitButton;
+
+//------------------------------------- Functions -------------------------------------
+function badScore(points) {
+    if (points >= 3) {
+
+    }
+}
+
+function goodScore(points) {
+    if (points >= 5 & points <= 3) {
+        
+    }
+}
+
+function perfectScore(points) {
+    if (points === 10) {
+        
+    }
+}
+
+//------------------------------ Event listeners / Buttons ------------------------------
+
+
