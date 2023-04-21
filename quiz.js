3280a05c2b362fed1098d5658fdbce2796d04558
Neo Babic <neo.babic@elev.ga.ntig.se>
2023-04-21 14:58:43 +0200

    amogus

--- quiz.js
+++ quiz.js
@@ -1,25 +1,43 @@
 //----------------------------------- Lets for quiz -----------------------------------
 let points = 0;
 let hiddenQuestion = 0;
-let q1 = document.querySelector(".q1");
-let q2 = document.querySelector(".q2");
 
+let q1 = document.getElementById("q1");
+let q2 = document.getElementById("q2");
+let q3 = document.getElementById("q3");
+let questionIndex = 0;
 //----------------------------------- Lets for buttons --------------------------------
+let backButton = document.querySelector(".backButton");
 let nextButton = document.querySelector(".nextButton");
 let finishButton = document.querySelector(".finishButton");
 
 //----------------------------------- Event Listeners ---------------------------------
 nextButton.addEventListener("click", nextQuestion);
 
+//----------------------------------- Lists -------------------------------------------
+let questions = [];
+questions.push(q1);
+questions.push(q2);
+questions.push(q3);
 
 //------------------------------------- Functions -------------------------------------
 function nextQuestion() {
-    q1.classList.toggle("hidden");
-    q2.classList.toggle("hidden");
+    questionIndex = (questionIndex + 1) % questions.length;
+    questionShowed(questionIndex);
 }
 
-function finishQuiz () {
 
+
+function questionShowed(index) {
+    for (let i = 0; i < questions.length; i++) {
+        if (i == index) {
+            questions[i].style.display = "block";
+        }
+        else {
+            questions[i].style.display = "none";
+        }
+    }
 }
+
 
 
